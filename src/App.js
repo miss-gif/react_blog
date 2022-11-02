c3ce0367d1e2478f7f132475abb45ab69b283e89
miss <ddc.akari@gmail.com>
2022-11-02 23:43:41 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let test = "text";
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div style={{ color: "blue", fontSize: "30px" }}>개발 blog</div>
+        <div> {test} </div>
       </div>
     </div>
   );
